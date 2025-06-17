ec82bd9941a3393fcc685b4570953487f78d7bf1
Campbell <crc00042@mix.wvu.edu>
2025-06-17 11:49:30 -0600

    fix .pt path

--- job_runs/flowmm_benchmark_alex/write_benchmark.sh
+++ job_runs/flowmm_benchmark_alex/write_benchmark.sh
@@ -2,7 +2,7 @@
 conda activate flowmm
 
 python inspect_pt.py \
-   --pt_path /lab/mml/kipp/677/jarvis/rhys/benchmarks/job_runs/flowmm_benchmark_test/outputs/rfmcsp-conditional-supercon/cxkrfmgi/checkpoints/inferences/consolidated_reconstruct.pt \
+   --pt_path /lab/mml/kipp/677/jarvis/rhys/benchmarks/job_runs/flowmm_benchmark_alex/outputs/rfmcsp-conditional-alexandria/cxkrfmgi/checkpoints/inferences/consolidated_reconstruct.pt \
    --output_csv AI-AtomGen-prop-dft_3d-test-rmse.csv
 
 mv ../../models/flowmm/AI-AtomGen-prop-dft_3d-test-rmse.csv .
