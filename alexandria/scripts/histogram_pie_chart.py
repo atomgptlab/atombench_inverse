3b0b5d344f66339076ca033b891cb7b7c53d10f0
Charles Rhys Campbell <crc8@mr-french.nist.gov>
2025-06-27 15:14:57 -0400

    one decimal place

--- alexandria/scripts/histogram_pie_chart.py
+++ alexandria/scripts/histogram_pie_chart.py
@@ -171,8 +171,9 @@ def create_composition_pie_chart(df: pd.DataFrame, output_dir: Path) -> None:
         counts,
         labels=labels,           # ← length now matches counts
         labeldistance=1.3,
-	radius=1,
-	shadow=False,
+	    autopct='%1.1f%%',
+        radius=1,
+	    shadow=False,
         startangle=90,
         wedgeprops={"edgecolor": "w", "linewidth": 1},
         textprops={"fontsize": 12},
