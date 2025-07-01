2f2a840a24b6f39779ef8adc8325cef04405f8a0
Charles Campbell <crc00042@mix.wvu.edu>
2025-07-01 13:52:29 -0600

    add json

--- job_runs/loop.sh
+++ job_runs/loop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for dir in */
 	do cd "$dir"
-		rm -rf distribution.pdf metrics.txt
-		python ../../scripts/plot_error_distribution.py | tail -n 30 > metrics.txt
+		rm -rf distribution.pdf metrics.json
+		python ../../scripts/plot_error_distribution.py #| tail -n 30 > metrics.txt
 		cd ..
 done
 python ../scripts/json_to_csv.py
