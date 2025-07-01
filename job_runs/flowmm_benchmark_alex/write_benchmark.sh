12b0cc40a784e381ab450aaf5b0e76e120555fd2
Charles Campbell <crc00042@mix.wvu.edu>
2025-07-01 14:04:56 -0600

    rm metrics.csv script before making a new one

--- job_runs/flowmm_benchmark_alex/write_benchmark.sh
+++ job_runs/flowmm_benchmark_alex/write_benchmark.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 conda activate flowmm
 
+rm -f AI-AtomGen*
+
 python inspect_pt.py \
-   --pt_path /lab/mml/kipp/677/jarvis/rhys/benchmarks/job_runs/flowmm_benchmark_alex/outputs/rfmcsp-conditional-supercon/u8xsq8qk/checkpoints/inferences/consolidated_reconstruct.pt \
+   --pt_path /lab/mml/kipp/677/jarvis/rhys/benchmarks/job_runs/flowmm_benchmark_alex/outputs/rfmcsp-conditional-alexandria/u8xsq8qk/checkpoints/inferences/consolidated_reconstruct.pt \
    --output_csv AI-AtomGen-prop-dft_3d-test-rmse.csv
 
 mv ../../models/flowmm/AI-AtomGen-prop-dft_3d-test-rmse.csv .
