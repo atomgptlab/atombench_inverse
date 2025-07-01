cd7597c6e78b42c83ea8e76e457b2659bf1bf7fc
Campbell <crc00042@mix.wvu.edu>
2025-07-01 08:37:38 -0600

    make loop.sh have multiple lines

---
+++ job_runs/loop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for dir in */
+	do cd "$dir"
+		rm distribution.pdf metrics.txt
+		python ../../scripts/plot_error_distribution.py | tail -n 30 > metrics.txt
+		cd ..
+done
+python ../scripts/text_to_csv.py
