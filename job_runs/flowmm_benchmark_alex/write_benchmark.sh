375c798a770f5619b0c002e7410fe45c09533f2e
Charles Campbell <crc00042@mix.wvu.edu>
2025-07-01 12:34:56 -0600

    test commit

--- job_runs/flowmm_benchmark_alex/write_benchmark.sh
+++ job_runs/flowmm_benchmark_alex/write_benchmark.sh
@@ -2,7 +2,9 @@
 conda activate flowmm
 
 python inspect_pt.py \
-   --pt_path /lab/mml/kipp/677/jarvis/rhys/benchmarks/job_runs/flowmm_benchmark_alex/outputs/rfmcsp-conditional-supercon/gllya480/checkpoints/inferences/consolidated_reconstruct.pt \
+   --pt_path /lab/mml/kipp/677/jarvis/rhys/benchmarks/job_runs/flowmm_benchmark_alex/outputs/rfmcsp-conditional-supercon/u8xsq8qk/checkpoints/inferences/consolidated_reconstruct.pt \
    --output_csv AI-AtomGen-prop-dft_3d-test-rmse.csv
 
 mv ../../models/flowmm/AI-AtomGen-prop-dft_3d-test-rmse.csv .
+
+#comment to test git push via PyCharm
