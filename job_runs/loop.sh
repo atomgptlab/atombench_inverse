3c2408ac25a919252ed3d32bc1b9988409bed18b
Campbell <crc00042@mix.wvu.edu>
2025-07-01 08:38:55 -0600

    rm -rf

--- job_runs/loop.sh
+++ job_runs/loop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for dir in */
-	do cd "$dir"
-		rm distribution.pdf metrics.txt
+	fo cd "$dir"
+		rm -rf distribution.pdf metrics.txt
 		python ../../scripts/plot_error_distribution.py | tail -n 30 > metrics.txt
 		cd ..
 done
