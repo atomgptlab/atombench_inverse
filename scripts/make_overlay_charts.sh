557aebd5a89fa5b6664ac13459ed07273ac19dde
crhysc <crc00042@mix.wvu.edu>
2025-09-11 14:39:41 -0400

    successfully made two new charts

--- scripts/make_overlay_charts.sh
+++ scripts/make_overlay_charts.sh
@@ -4,6 +4,7 @@
 python scripts/overlay_compare.py \
   --alex-csv-files alexandria/dataset1.csv alexandria/dataset2.csv \
   --jarvis-dataset supercon_3d \
+  --jarvis-target-key Tc \
   --output ./overlay_outputs \
   --tc-min 0 --tc-max 45 --tc-step 1.5
 
