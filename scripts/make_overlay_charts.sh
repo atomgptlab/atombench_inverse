b18b8f724c74f5c623d39c6bf58154cb3cceaac7
crhysc <crc00042@mix.wvu.edu>
2025-09-11 13:58:50 -0400

    fix path

--- scripts/make_overlay_charts.sh
+++ scripts/make_overlay_charts.sh
@@ -2,7 +2,7 @@
 
 
 python scripts/overlay_compare.py \
-  --alex-csv-files alexandria/dataset1.csv dataset2.csv \
+  --alex-csv-files alexandria/dataset1.csv alexandria/dataset2.csv \
   --jarvis-dataset dft_3d \
   --output ./overlay_outputs \
   --tc-min 0 --tc-max 45 --tc-step 1.5
