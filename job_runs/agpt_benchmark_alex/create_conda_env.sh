1b2167aa449b332493846d514d756be0d305b07d
Charles Campbell <crc00042@mix.wvu.edu>
2025-09-25 12:26:03 -0400

    fix paths

--- job_runs/agpt_benchmark_alex/create_conda_env.sh
+++ job_runs/agpt_benchmark_alex/create_conda_env.sh
@@ -6,4 +6,4 @@ conda create --name my_atomgpt python=3.10 -y
 conda activate my_atomgpt
 
 pip install uv
-uv pip install -e .
+uv pip install -e models/atomgpt
--- job_runs/agpt_benchmark_jarvis/create_conda_env.sh
+++ job_runs/agpt_benchmark_jarvis/create_conda_env.sh
@@ -6,4 +6,5 @@ conda create --name my_atomgpt python=3.10 -y
 conda activate my_atomgpt
 
 pip install uv
-uv pip install -e .
+uv pip install -e models/atomgpt
+
